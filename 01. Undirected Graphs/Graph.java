bb336c4fff6a845d8d38f6c384432fec18ceb826
mojito <msharma3@me.ittr.ac.in>
2021-02-04 13:05:35 +0530

    Undirected Graphs

---
+++ 01. Undirected Graphs/Graph.java
@@ -0,0 +1,22 @@
+/**
+* <h1>Undirected Graphs: Set of vertices connected pairwise by edges. Challenging branch of computer science</h1>
+* 
+* <li> Applications:
+* <li> Protein-Protein Interaction Network
+* <li> The Internet as mapped by the Opte Project
+* <li> Map of science clickstreams
+* <li> Visualising friendships by Paul Butler
+* <li> Enron Email dataset
+* <li> The evolution of FCC-lobbying coalition
+* <li> Framingham Heart Study
+* <li> Graph=Neural Network, Vertex=Neuron, Edge=Synapse
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   04-02-2021
+* 
+*/
+
+public class Graph {
+
+}
