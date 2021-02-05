c871b44fcf4e0e75c7319e5a91094aa1c53046d9
mojito <msharma3@me.ittr.ac.in>
2021-02-05 16:21:24 +0530

    Strongly Connected Components

---
+++ 02. Directed Graphs/KosarajuSharirSCC.java
@@ -0,0 +1,17 @@
+/**
+* <h1>Strongly Connected Components: Vertices v & w are strongly connected components if there is a dedicated path
+* 									 from v to w and a directed path from w to v. </h1>
+* 
+* <li> It is an equivalence relation: Reflexive, Transitive, Symmetric
+* <li> A strong component is a maximal subset of strongly connected vertices.
+* <li> A DAG with V vertices, E edges has V strongly connected components.
+* 
+* @author  Mohit Sharma
+* @version 3.0
+* @since   05-02-2021
+* 
+*/
+
+public class KosarajuSharirSCC {
+
+}
