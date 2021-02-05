d4242f0ef9dcc307b8734d88ee94b6adeee324b6
mojito <msharma3@me.ittr.ac.in>
2021-02-05 13:07:48 +0530

    Web Crawler

---
+++ 02. Directed Graphs/WebCrawler.java
@@ -0,0 +1,19 @@
+/**
+* <h1>Web Crawler: Digraph BFS application. Crawl Internet, starting from some root website. </h1>
+* 
+* <li> The Internet is a digraph. Use BFS with implicit digraph
+* <li> Start at some root website (say https://www.princeton.edu)
+* <li> Maintain a Queue of websites to explore
+* <li> Maintain a SET of discovered websites.
+* <li> Dequeue the next website and enqueue websites to which it links (provided you haven't done so before)
+* <li> Why not DFS? Internet is not fixed (some pages generate new ones when visited and this could trap search)
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   05-02-2021
+* 
+*/
+
+public class WebCrawler {
+
+}
