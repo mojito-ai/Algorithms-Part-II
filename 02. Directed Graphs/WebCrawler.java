85fd6237ee5709c730074e591929d3d13d146d1e
mojito <msharma3@me.ittr.ac.in>
2021-02-05 13:15:37 +0530

    Web 2.0 (Documentation Update)

--- 02. Directed Graphs/WebCrawler.java
+++ 02. Directed Graphs/WebCrawler.java
@@ -23,27 +23,27 @@ public class WebCrawler {
 	
 	public static void main(String [] args)
 	{
-		Queue<String> q=new Queue<>();
-		SET<String> visited=new SET<>();
+		Queue<String> q=new Queue<>(); //queue of sites to crawl
+		SET<String> visited=new SET<>(); //set of visited sites
 		
 		String s="https://www.princeton.edu";
-		q.enqueue(s);
+		q.enqueue(s); //start crawling from s
 		visited.add(s);
 		
 		while(!q.isEmpty())
 		{
-			String v=q.dequeue();
+			String v=q.dequeue();	//read in raw html for next site in queue
 			System.out.println(v);
 			In in=new In(v);
 			String input=in.readAll();
 			
-			String regexp = "http://(\\w+\\.)*(\\w+)";
-			Pattern pattern=Pattern.compile(regexp);
+			String regexp = "http://(\\w+\\.)*(\\w+)"; //http://xxx.yyy.zzz
+			Pattern pattern=Pattern.compile(regexp); //use regular expressions to find all URLs in site
 			Matcher matcher=pattern.matcher(input);
 			while(matcher.find())
 			{
 				String w=matcher.group();
-				if(!visited.contains(w))
+				if(!visited.contains(w))		//if unvisited, mark v as visited and put on queue
 				{
 					visited.add(w);
 					q.enqueue(w);
