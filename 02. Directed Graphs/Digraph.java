f5412ba0876ccd71ea55e6aeac8b2b2aa694a539
mojito <msharma3@me.ittr.ac.in>
2021-02-05 12:03:15 +0530

    Directed Graph Applications

---
+++ 02. Directed Graphs/Digraph.java
@@ -0,0 +1,24 @@
+/**
+* <h1>Directed Graph: Set of vertices connected pairwise by directed edges.</h1>
+* 
+* <li> Applications:
+* <li> Road Network (vertex=intersection, edge=one way street)
+* <li> Political Blogosphere graph (vertex=political blog, edge=link) The political blogosphere graph & the 2004 US Election-Divided they blog, Adamic & Glance
+* <li> Overnight interbank loan graph (vertex=bank, edge=overnight loan) The topology of the Federal Funds Market, Bech & Atalay
+* <li> Implication Graph (vertex=variable, edge=logical implication)
+* <li> Combinational Circuit (vertex=logical gate, edge=wire)
+* <li> Word Net (vertex=synset, edge=hypernym relationship)
+* <li> The McChrystal Afghanistan Powerpoint slide
+* <li> Inheritance graph (vertex=class, edge=inherits from)
+* <li> Object graph (vertex=object, edge=pointer)
+* <li> Web (vertex=web page, edge=hyperlink)
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   04-02-2021
+* 
+*/
+
+public class Digraph {
+
+}
