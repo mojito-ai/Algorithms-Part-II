34c75e05b756b7ca558d32a9f1494ddd9ccba5ed
mojito <msharma3@me.ittr.ac.in>
2021-02-05 13:12:52 +0530

    Web v1.0

--- 02. Directed Graphs/WebCrawler.java
+++ 02. Directed Graphs/WebCrawler.java
@@ -1,3 +1,8 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import edu.princeton.cs.algs4.In;
+
 /**
 * <h1>Web Crawler: Digraph BFS application. Crawl Internet, starting from some root website. </h1>
 * 
@@ -15,5 +20,36 @@
 */
 
 public class WebCrawler {
-
+	
+	public static void main(String [] args)
+	{
+		Queue<String> q=new Queue<>();
+		SET<String> visited=new SET<>();
+		
+		String s="https://www.princeton.edu";
+		q.enqueue(s);
+		visited.add(s);
+		
+		while(!q.isEmpty())
+		{
+			String v=q.dequeue();
+			System.out.println(v);
+			In in=new In(v);
+			String input=in.readAll();
+			
+			String regexp = "http://(\\w+\\.)*(\\w+)";
+			Pattern pattern=Pattern.compile(regexp);
+			Matcher matcher=pattern.matcher(input);
+			while(matcher.find())
+			{
+				String w=matcher.group();
+				if(!visited.contains(w))
+				{
+					visited.add(w);
+					q.enqueue(w);
+				}
+			}
+			
+		}
+	}
 }
