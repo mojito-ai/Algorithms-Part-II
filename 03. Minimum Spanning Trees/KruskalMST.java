c26cbb01bc3c562663cc5359058b98d904f72537
mojito <msharma3@me.ittr.ac.in>
2021-02-06 12:28:12 +0530

    MST API v1.0

---
+++ 03. Minimum Spanning Trees/KruskalMST.java
@@ -0,0 +1,29 @@
+/**
+* <h1>Kruskal's Algorithm: Consider edges in ascending order of weight. Add edge to tree T unless doing so would create a cycle</h1>
+* 
+* <li> In visualisation, its taking the small edges and they coalless together in little clusters & eventually get longer and longer and connect together
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   06-02-2021
+* 
+*/
+
+public class KruskalMST {
+	
+	KruskalMST(EdgeWeightedGraph G)
+	{
+		
+	}
+	
+	Iterable<Edge> edges()
+	{
+		
+	}
+	
+	double weight()
+	{
+		
+	}
+
+}
