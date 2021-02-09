13681ba6fe36ec3fad248eb1416141abdf1d7dce
mojito <msharma3@me.ittr.ac.in>
2021-02-09 16:46:50 +0530

    DirectedEdge API

---
+++ 04. Shortest Paths/DirectedEdge.java
@@ -0,0 +1,41 @@
+/**
+* <h1>Directed Edge: Weighted Directed Edge abstraction needed for Directed Edge weighted Graph construction</h1>
+* 
+* <li> Edges should be Comparable type
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   09-02-2021
+* 
+*/
+
+public class DirectedEdge implements Comparable<DirectedEdge>
+{
+	DirectedEdge(int v, int w, double weight)
+	{
+		
+	}
+	
+	int from()
+	{
+		
+	}
+	
+	int to()
+	{
+		
+	}
+	
+	double weight()
+	{
+		
+	}
+	
+	@Override
+	public int compareTo(DirectedEdge o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+}
