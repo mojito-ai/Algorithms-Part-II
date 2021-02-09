286a8d9a7ccdc5ec8a42a1bbf81d0da154d7eab5
mojito <msharma3@me.ittr.ac.in>
2021-02-09 16:55:29 +0530

    EdgeWeightedDigraph API

---
+++ 04. Shortest Paths/EdgeWeightedDigraph.java
@@ -0,0 +1,44 @@
+/**
+* <h1>Edge Weighted Digraph: API to create an adjacency list of Directed Edges</h1>
+* 
+* <li> Can allow self loops and parallel edges
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   09-02-2021
+* 
+*/
+
+public class EdgeWeightedDigraph 
+{
+	EdgeWeightedDigraph(int V)
+	{
+		
+	}
+	
+	void addEdge(DirectedEdge e)
+	{
+		
+	}
+	
+	Iterable<DirectedEdge> adj(int v)
+	{
+		
+	}
+	
+	int V()
+	{
+		
+	}
+	
+	int E()
+	{
+		
+	}
+	
+	Iterable<DirectedEdge> edges()
+	{
+		
+	}
+
+}
