05816e3fd4f7592c07d9a2e34234af1ccc6aa937
mojito <msharma3@me.ittr.ac.in>
2021-02-09 18:48:34 +0530

    DijkstraSP v1.0

---
+++ 04. Shortest Paths/DijkstraSP.java
@@ -0,0 +1,14 @@
+/**
+* <h1>Dijkstra's Algorithm</h1>
+* <li> Consider vertices in increasing order of distance from s (non tree vertex with the lowest distTo[] value)
+* <li> Add vertex to tree and relax all edges pointing from that vertex
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   09-02-2021
+* 
+*/
+
+public class DijkstraSP {
+
+}
