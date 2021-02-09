9135131daf87508dd1e53782493445453e579d5b
mojito <msharma3@me.ittr.ac.in>
2021-02-09 22:50:38 +0530

    No directed cycle SP

---
+++ 04. Shortest Paths/AcyclicSP.java
@@ -0,0 +1,16 @@
+/**
+* <h1>Edge Weighted DAGs: Acyclic edge weighted digraph -> It is easier to find shortest paths in an edge weighted digraph
+* 						  that has no directed cycles.</h1>
+* <li> Topological Sort algorithm:
+* <li> Consider vertices in topological order
+* <li> Relax all edges pointing from that vertex.
+* 
+* @author  Mohit Sharma
+* @version 2.0
+* @since   09-02-2021
+* 
+*/
+
+public class AcyclicSP {
+
+}
