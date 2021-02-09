c317da2c2ab8eee0e43a8e0f3ac582fa9e318a46
mojito <msharma3@me.ittr.ac.in>
2021-02-09 17:22:58 +0530

    Shortest Paths API

---
+++ 04. Shortest Paths/SP.java
@@ -0,0 +1,34 @@
+/**
+* <h1>Single-source shortest paths API: Find the shortest path from s to every other vertex.</h1>
+* 
+* <li>
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   09-02-2021
+* 
+*/
+
+public class SP 
+{
+	SP(EdgeWeightedDigraph G, int s)
+	{
+		
+	}
+	
+	double distTo(int v)
+	{
+		
+	}
+	
+	Iterable<DirectedEdge> pathTo(int v)
+	{
+		
+	}
+	
+	boolean hasPathTo(int v)
+	{
+		
+	}
+
+}
