a96acaba077d091d86227ef69f6c15323c7d3a9f
mojito <msharma3@me.ittr.ac.in>
2021-02-09 18:55:20 +0530

    Dijkstra's Quotes

--- 04. Shortest Paths/DijkstraSP.java
+++ 04. Shortest Paths/DijkstraSP.java
@@ -9,6 +9,15 @@
 * 
 */
 
+/*
+ * Edsger W. Dijkstra [Turing Award 1972]
+ * 1. Do only what only you can do
+ * 2. In their capacity as a tool, computers will be but a ripple on the surface of our culture. In their capacity as intellectual challenge, they are without precedent in the cultural history of mankind.
+ * 3. The use of COBOL cripples the mind; its teaching should, therefore, be regarded as a criminal offense.
+ * 4. It is practically impossible to teach good programming to students that have had a prior exposure to BASIC: as potential programmers they are mentally mutilated beyond hope of regeneration.
+ * 5. APL is a mistake, carried through to perfection. It is the language of the future for the programming techniques of the past: it creates a new generation of coding bums.
+ * 6. Object-oriented programming is an exceptionally bad idea which could only have originated in California.
+ */
 public class DijkstraSP {
 
 }
