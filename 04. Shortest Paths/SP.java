13fbccef60226cbfd6d0a79fa698e9b71e5bccca
mojito <msharma3@me.ittr.ac.in>
2021-02-09 17:33:09 +0530

    Shortest Paths v1.0

--- 04. Shortest Paths/SP.java
+++ 04. Shortest Paths/SP.java
@@ -11,24 +11,33 @@
 
 public class SP 
 {
+	private double [] distTo;
+	private DirectedEdge [] edgeTo;
 	SP(EdgeWeightedDigraph G, int s)
 	{
+		distTo=new double[G.V()];
+		edgeTo=new DirectedEdge[G.V()];
 		
 	}
 	
 	double distTo(int v)
 	{
-		
+		return distTo[v];
 	}
 	
 	Iterable<DirectedEdge> pathTo(int v)
 	{
-		
+		Stack<DirectedEdge> path=new Stack<>();
+		for(DirectedEdge e=edgeTo[v]; e!=null; e=edgeTo[e.from()])
+		{
+			path.push(e);
+		}
+		return path;
 	}
 	
 	boolean hasPathTo(int v)
 	{
-		
+		return edgeTo[v].weight()!=Double.POSITIVE_INFINITY;
 	}
 
 }
