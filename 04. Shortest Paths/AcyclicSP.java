3a603c8e16cce9312ba4abe9a188e2ce9d18d95d
mojito <msharma3@me.ittr.ac.in>
2021-02-09 23:16:23 +0530

    AcyclicSP v2.0

--- 04. Shortest Paths/AcyclicSP.java
+++ 04. Shortest Paths/AcyclicSP.java
@@ -7,7 +7,7 @@ import edu.princeton.cs.algs4.IndexMinPQ;
 * <li> Relax all edges pointing from that vertex.
 * 
 * @author  Mohit Sharma
-* @version 1.0
+* @version 2.0
 * @since   09-02-2021
 * 
 */
@@ -18,6 +18,15 @@ public class AcyclicSP {
 	private DirectedEdge [] edgeTo;
 	private final IndexMinPQ<Double> pq;
 	
+	/**
+	 * Proposition: Topological sort algorithm computes the SPT in any edge weighted DAG in time E+V
+	 * Proof: Each edge e=v->w is relaxed exactly once (when v is relaxed) leaving distTo[w]<=distTo[v]+e.weight()
+	 * <li> Inequality holds until algorithm terminates because: 
+	 * <li> distTo[w] cannot increase -> distTo[] are monotone decreasing
+	 * <li> distTo[v] will not change -> because of topological order, no edge pointing to v will be relaxed after v is relaxed.
+	 * @param G
+	 * @param s
+	 */
 	AcyclicSP(EdgeWeightedDigraph G, int s)
 	{
 		distTo=new double[G.V()];
