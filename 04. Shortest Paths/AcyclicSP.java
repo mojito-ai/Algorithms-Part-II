47b33702d0404033e81fde99152eeb16c1cb344d
mojito <msharma3@me.ittr.ac.in>
2021-02-09 22:55:03 +0530

    AcyclicSP API

--- 04. Shortest Paths/AcyclicSP.java
+++ 04. Shortest Paths/AcyclicSP.java
@@ -6,11 +6,38 @@
 * <li> Relax all edges pointing from that vertex.
 * 
 * @author  Mohit Sharma
-* @version 2.0
+* @version 1.0
 * @since   09-02-2021
 * 
 */
 
 public class AcyclicSP {
+	
+	private double [] distTo;
+	private DirectedEdge [] edgeTo;
+	
+	AcyclicSP(EdgeWeightedDigraph G, int s)
+	{
+		distTo=new double[G.V()];
+		edgeTo=new DirectedEdge[G.V()];
+	}
+	
+	Iterable<DirectedEdge> pathTo(int v)
+	{
+		Stack<DirectedEdge> path=new Stack<>();
+		for(DirectedEdge e=edgeTo[v]; e!=null; e=edgeTo[e.from()])
+			path.push(e);
+		return path;
+	}
+	
+	double distTo(int v)
+	{
+		return distTo[v];
+	}
+	
+	boolean hasPathTo(int v)
+	{
+		return distTo[v]!=Double.POSITIVE_INFINITY;
+	}
 
 }
