f44b2e240752eb8e8e5e7937d17322af3d1d0d5f
mojito <msharma3@me.ittr.ac.in>
2021-02-10 11:20:42 +0530

    BellmanFord Algorithm

--- 04. Shortest Paths/BellmanFordSP.java
+++ 04. Shortest Paths/BellmanFordSP.java
@@ -16,9 +16,26 @@ public class BellmanFordSP {
 	private final DirectedEdge [] edgeTo;
 	private final IndexMinPQ<Double> pq;
 	
+	/**
+	 * Initialize distTo[s]=0 & distTo[v]=infinity for all other vertices
+	 * <li> Relax all edges
+	 * @param G
+	 * @param s
+	 */
 	BellmanFordSP(EdgeWeightedDigraph G, int s)
 	{
+		distTo=new double[G.V()];
+		edgeTo=new DirectedEdge[G.V()];
+		pq=new IndexMinPQ<>(G.V());
 		
+		for(int v=0;v<G.V();v++)
+			distTo[v]=Double.POSITIVE_INFINITY;
+		distTo[s]=0.0;
+		
+		for(int i=0;i<G.V();i++)
+			for(int v=0;v<G.V();v++)
+				for(DirectedEdge e: G.adj(v))
+					relax(e);
 	}
 	
 	private void relax(DirectedEdge e)
