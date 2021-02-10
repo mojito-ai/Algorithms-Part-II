13ed962dce88010f28c3f1102c77d4dbef6b6948
mojito <msharma3@me.ittr.ac.in>
2021-02-10 11:06:47 +0530

    TopologicalLP v1.0

--- 04. Shortest Paths/TopologicalLP.java
+++ 04. Shortest Paths/TopologicalLP.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.IndexMinPQ;
 /**
 * <h1> Longest Paths in edge-weighted-DAGs: Formulate as a shortest paths problem in edge weighted DAGs</h1>
 * <li> Negate all weights
@@ -12,5 +13,54 @@
 */
 
 public class TopologicalLP {
+	private final double [] distTo;
+	private final DirectedEdge [] edgeTo;
+	private final IndexMinPQ<Double> pq;
+	
+	TopologicalLP(EdgeWeightedDigraph G, int s)
+	{
+		distTo=new double[G.V()];
+		edgeTo=new DirectedEdge[G.V()];
+		pq=new IndexMinPQ<>(G.V());
+		for(int v=0;v<G.V();v++)
+			distTo[v]=Double.NEGATIVE_INFINITY;
+		distTo[s]=0.0;
+		
+		Topological topological=new Topological(G);
+		for(int v: topological.order())
+			for(DirectedEdge w:G.adj(v))
+				relax(w);
+	}
+	
+	private void relax(DirectedEdge e)
+	{
+		int v=e.from();
+		int w=e.to();
+		if(distTo[w]>distTo[v]+e.weight())
+		{
+			distTo[w]=distTo[v]+e.weight();
+			edgeTo[w]=e;
+			if(pq.contains(w))	pq.decreaseKey(w, distTo[w]);
+			else				pq.insert(w, distTo[w]);
+		}
+	}
+	
+	Iterable<DirectedEdge> pathTo(int v)
+	{
+		Stack<DirectedEdge> path=new Stack<>();
+		for(DirectedEdge e=edgeTo[v]; e!=null; e=edgeTo[e.from()])
+			path.push(e);
+		return path;
+	}
+	
+	double distTo(int v)
+	{
+		return -distTo[v];
+	}
+	
+	boolean hasPathTo(int v)
+	{
+		return distTo[v]!=Double.NEGATIVE_INFINITY;
+	}
 
 }
