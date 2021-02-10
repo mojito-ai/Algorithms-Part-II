0ad8e8d521ceb0e67127d402787d8c8beda5ff78
mojito <msharma3@me.ittr.ac.in>
2021-02-10 10:55:18 +0530

    Longest Paths in DAGs

---
+++ 04. Shortest Paths/TopologicalLP.java
@@ -0,0 +1,16 @@
+/**
+* <h1> Longest Paths in edge-weighted-DAGs: Formulate as a shortest paths problem in edge weighted DAGs</h1>
+* <li> Negate all weights
+* <li> Find shortest paths
+* <li> Negate weights in result
+* <li> Essentially negative sense of equality in relax
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   10-02-2021
+* 
+*/
+
+public class TopologicalLP {
+
+}
