21b35124eb952f09cbaebce8d3e36bfd025549ff
mojito <msharma3@me.ittr.ac.in>
2021-02-10 11:17:30 +0530

    BellmanFordSP API

--- 04. Shortest Paths/BellmanFordSP.java
+++ 04. Shortest Paths/BellmanFordSP.java
@@ -11,5 +11,45 @@ import edu.princeton.cs.algs4.IndexMinPQ;
 */
 
 public class BellmanFordSP {
+	
+	private final double [] distTo;
+	private final DirectedEdge [] edgeTo;
+	private final IndexMinPQ<Double> pq;
+	
+	BellmanFordSP(EdgeWeightedDigraph G, int s)
+	{
+		
+	}
+	
+	private void relax(DirectedEdge e)
+	{
+		int v=e.from();
+		int w=e.to();
+		if(distTo[w]>distTo[v]+e.weight())
+		{
+			distTo[w]=distTo[v]+e.weight();
+			edgeTo[w]=e;
+			if(pq.contains(w))	pq.decreaseKey(w, distTo[w]);
+			else				pq.insert(w, distTo[w]);
+		}
+	}
+	
+	Iterable<DirectedEdge> pathTo(int v)
+	{
+		Stack<DirectedEdge> path=new Stack<>();
+		for(DirectedEdge e=edgeTo[v]; e!=null; e=edgeTo[e.from()])
+			path.push(e);
+		return path;
+	}
+	
+	double distTo(int v)
+	{
+		return distTo[v];
+	}
+	
+	boolean hasPathTo(int v)
+	{
+		return distTo[v]!=Double.POSITIVE_INFINITY;
+	}
 
 }
