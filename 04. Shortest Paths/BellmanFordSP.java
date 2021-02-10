a00d9f1c18c67981a435366a0dc02b7df65458ab
mojito <msharma3@me.ittr.ac.in>
2021-02-10 11:15:37 +0530

    Negative Weights

---
+++ 04. Shortest Paths/BellmanFordSP.java
@@ -0,0 +1,15 @@
+import edu.princeton.cs.algs4.IndexMinPQ;
+/**
+* <h1> Negative weights: Shortest paths with negative weights is complex and sometimes intractable </h1>
+* <li> Failed attempts: Dijkstra doesen't work with negative weights
+* <li> Failed attempts: Add a constant to every edge weight doesen't work.
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   10-02-2021
+* 
+*/
+
+public class BellmanFordSP {
+
+}
