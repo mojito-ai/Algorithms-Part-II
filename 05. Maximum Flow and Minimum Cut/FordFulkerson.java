5aece404e40da64fd9ae3be0ca97dd52076db1be
mojito <msharma3@me.ittr.ac.in>
2021-02-11 19:04:59 +0530

    Augmenting Paths

---
+++ 05. Maximum Flow and Minimum Cut/FordFulkerson.java
@@ -0,0 +1,18 @@
+/**
+* <h1>Ford Fulkerson Algorithm: </h1>
+* <li> Initialization: Start with 0 flow
+* <li> Idea: Inrease flow along augmenting paths
+* <li> Augmenting paths: Find an undirected path from s to t such that:
+* <li> Can increase flow on forward edges (not full)
+* <li> Can decrease flow on backward edges (not empty)
+* <li> When we augment, we preserve local equilibrium at vertex property
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   11-02-2021
+* 
+*/
+
+public class FordFulkerson {
+
+}
