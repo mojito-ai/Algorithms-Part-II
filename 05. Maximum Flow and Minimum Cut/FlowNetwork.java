96815f623b95f6610b71479570dc764ab3a1fae3
mojito <msharma3@me.ittr.ac.in>
2021-02-11 21:46:58 +0530

    Flow Network

---
+++ 05. Maximum Flow and Minimum Cut/FlowNetwork.java
@@ -0,0 +1,15 @@
+/**
+* <h1>Flow Network: Need to process edge e=v->w in either directions. Include e in both v and w's adjacency list</h1>
+* 
+* <li> Residual Network: A useful view of a flow network
+* <li> Augmenting path in orignal network is equivalent to directed path in residual network.
+* 
+* @author  Mohit Sharma
+* @version 2.0
+* @since   11-02-2021
+* 
+*/
+
+public class FlowNetwork {
+
+}
