30905147363976655d565f0cb11ade38204496ba
mojito <msharma3@me.ittr.ac.in>
2021-02-11 21:54:32 +0530

    FlowNetwork v1.0

--- 05. Maximum Flow and Minimum Cut/FlowNetwork.java
+++ 05. Maximum Flow and Minimum Cut/FlowNetwork.java
@@ -1,3 +1,4 @@
+import java.util.TreeSet;
 /**
 * <h1>Flow Network: Need to process edge e=v->w in either directions. Include e in both v and w's adjacency list</h1>
 * 
@@ -12,35 +13,51 @@
 */
 
 public class FlowNetwork {
+	private final Bag<FlowEdge> [] adj;
+	private final int v;
 	
+	@SuppressWarnings("unchecked")
 	FlowNetwork(int V)
 	{
-		
+		this.v=V;
+		adj=(Bag<FlowEdge> []) new Bag[V];
+		for(int v=0;v<V;v++)
+			adj[v]=new Bag<FlowEdge> ();
 	}
 	
 	void addEdge(FlowEdge e)
 	{
-		
+		int v=e.from();
+		int w=e.to();
+		adj[v].add(e);	//add forward edge
+		adj[w].add(e);	//add backward edge
 	}
 	
 	Iterable<FlowEdge> adj(int v)
 	{
-		
+		return adj[v];
 	}
 	
 	Iterable<FlowEdge> edges()
 	{
-		
+		TreeSet<FlowEdge> edges=new TreeSet<>();
+		for(int v=0;v<V();v++)
+			for(FlowEdge e: adj(v))
+				edges.add(e);
+		return edges;
 	}
 	
 	int V()
 	{
-		
+		return v;
 	}
 	
 	int E()
 	{
-		
+		int e=0;
+		for(int v=0;v<V();v++)
+			e+=((Bag<FlowEdge>) adj(v)).size();
+		return e/2;
 	}
 
 }
