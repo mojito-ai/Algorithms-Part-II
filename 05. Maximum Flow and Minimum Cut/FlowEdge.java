6f41088fc1b59060351f8bcda41b8ba6235a7a87
mojito <msharma3@me.ittr.ac.in>
2021-02-11 21:32:24 +0530

    FlowEdge

---
+++ 05. Maximum Flow and Minimum Cut/FlowEdge.java
@@ -0,0 +1,19 @@
+/**
+* <h1>Flow-Edge: Associate flow fₑ and capacity cₑ wit edge e=v->w</h1>
+* 
+* <li> Residual capacity: 
+* <li> Forward edge = residual capacity = cₑ-fₑ
+* <li> Backward edge = residual capacity = fₑ
+* <li> Augment Flow:
+* <li> Forward edge: Add Δ
+* <li> Backward edge: Subtract Δ
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   11-02-2021
+* 
+*/
+
+public class FlowEdge {
+
+}
