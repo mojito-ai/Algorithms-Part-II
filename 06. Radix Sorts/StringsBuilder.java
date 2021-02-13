0ba2fae3591c916ab8b5e449840f10ba0518aa87
mojito <msharma3@me.ittr.ac.in>
2021-02-13 18:28:01 +0530

    StringBuilder

---
+++ 06. Radix Sorts/StringsBuilder.java
@@ -0,0 +1,16 @@
+/**
+* <h1>String Builder data type: Sequence of characters (mutable) </h1>
+* 
+* <li> Underlying implementation of resizing char [] array and length
+* <li> StringBuilder is similar to String, but thread safe and slower
+* <li> Substring take time ~ N as we have to create a new resizing array.
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   13-02-2021
+* 
+*/
+
+public class StringsBuilder {
+
+}
