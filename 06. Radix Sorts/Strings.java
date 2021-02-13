1a0485ba09dee5cdec9f9a92a770a0dda8428e04
mojito <msharma3@me.ittr.ac.in>
2021-02-13 18:11:14 +0530

    Strings in Java

---
+++ 06. Radix Sorts/Strings.java
@@ -0,0 +1,21 @@
+/**
+* <h1>Strings in Java: String is sequence of characters</h1>
+* 
+* <li> Important fundamental abstraction
+* <li> Information processing
+* <li> Genomic sequences
+* <li> Communication systems (e.g. Email)
+* <li> Programming systems (e.g. Java Programs)
+*
+* "The digital information that underlies biochemistry, cell biology, and development can be represented by a simple string of
+*  G's, A's, T's & C's. This string is the root data structure of an organism's biology" - M. V. Olson
+*  
+* @author  Mohit Sharma
+* @version 1.0
+* @since   13-02-2021
+* 
+*/
+
+public class Strings {
+
+}
