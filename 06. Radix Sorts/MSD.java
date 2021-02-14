70d42216d14135c0b326a304af67467e36397461
mojito <msharma3@me.ittr.ac.in>
2021-02-14 20:00:01 +0530

    MSD v1.0

--- 06. Radix Sorts/MSD.java
+++ 06. Radix Sorts/MSD.java
@@ -15,17 +15,39 @@ public class MSD {
 	
 	public static void sort (String [] a)
 	{
-		
+		String [] aux=new String[a.length];
+		sort(a,aux,0,a.length-1,0);
 	}
 	
 	private static void sort(String [] a, String [] aux, int lo, int hi, int d)
 	{
+		if(hi<=lo)	return;
 		
+		int R=256;
+		int N=a.length;
+		int [] count = new int[R+2];
+		
+		for(int i=lo; i<=hi; i++)
+			count[charAt(a[i],d) + 2]++;
+		
+		for(int r=0; r<R+1; r++)
+			count[r+1]+=count[r];
+		
+		for(int i=lo; i<=hi; i++)
+			aux[count[charAt(a[i],d)+1]++]=a[i];
+		
+		for(int i=lo; i<=hi; i++)
+			a[i]=aux[i-lo];
+		
+		for(int r=0; r<R; r++)
+			sort(a, aux, lo+count[r], lo+count[r+1]-1, d+1);
+
 	}
 	
 	/**
 	 * Variable length strings: Treat strings as if they had an extra char at the end (smaller than any char)
 	 * 
+	 * <li> C strings have an extra char \0 at the end so no extra -1 needed.
 	 * @param s
 	 * @param d
 	 * @return
