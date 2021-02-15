03aacd9801afc85da401eeabc4739a171cad311a
mojito <msharma3@me.ittr.ac.in>
2021-02-15 22:23:53 +0530

    TST search

--- 07. Tries/TST.java
+++ 07. Tries/TST.java
@@ -52,11 +52,19 @@ public class TST<Value> {
 
 	public Value get(String key)
 	{
-		
+		Node x=get(root, key, 0);
+		if(x==null)	return null;
+		return x.value;
 	}
 	private Node get(Node x, String key, int d)
 	{
-
+		if(x==null)	return null;
+		
+		char c=key.charAt(d);
+		if(c<x.c)					return get(x.left, key, d);
+		else if(c>x.c)				return get(x.right, key, d);
+		else if(d<key.length()-1)	return get(x.mid, key, d+1);
+		else						return x;
 	}
 	
 
