864669bda62f428567af9398cebef34534e60e45
mojito <msharma3@me.ittr.ac.in>
2021-02-15 11:30:08 +0530

    LongestRepeatedSubstring

---
+++ 06. Radix Sorts/LongestRepeatedSubstring.java
@@ -0,0 +1,18 @@
+/**
+* <h1> Longest Repeated Substring: Given a string of N characters, find the longest repeated substring
+* 
+* <li> Brute force: Try all indices i & j for start of possible match.
+* <li> Compute longest common prefix (LCP) for each pair.
+* <li> Running time: N^2/2 possible choices for i & j. Finding longest common prefix takes atmost D character compares. 
+* <li> Time ~ Dn^2 ; where D is the length of the longest common prefix
+* 
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   15-02-2021
+* 
+*/
+
+public class LongestRepeatedSubstring {
+
+}
