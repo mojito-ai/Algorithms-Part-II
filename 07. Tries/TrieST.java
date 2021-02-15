89814425f060dccc1cc5ffdca741ed66aa08e861
mojito <msharma3@me.ittr.ac.in>
2021-02-15 21:18:08 +0530

    Tries (Symbol Table Review-Key Comapres)

---
+++ 07. Tries/TrieST.java
@@ -0,0 +1,20 @@
+/**
+* <h1> R-way Tries: </h1>
+* 
+* <li> Summary of performance of symbol table implementations
+* 
+* <li> Order of growth of frequency of operations (key compares)
+* <li> Implementation		Search		Insert		Delete			Ordered Ops			Operation on keys
+* <li>	Red-Black BST		log N		 log N		 log N				Yes					compareTo()
+* <li>  HashTable			 1*			   1*		   1*				No					equals() hashCode()		* under uniform hashing assumption
+* 
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   15-02-2021
+* 
+*/
+
+public class TrieST {
+
+}
