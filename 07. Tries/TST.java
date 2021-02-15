5c20672e6be6734f806a1be0c7d4b49b61f3bc69
mojito <msharma3@me.ittr.ac.in>
2021-02-15 22:29:52 +0530

    TST Insert

--- 07. Tries/TST.java
+++ 07. Tries/TST.java
@@ -36,10 +36,23 @@ public class TST<Value> {
 	
 	void put(String key, Value val)
 	{
-		
+		root=put(root, key, val, 0);
 	}
 	private Node put(Node x, String key, Value val, int d)
 	{
+		char c=key.charAt(d);
+		if(x==null)		
+		{
+			x=new Node();
+			x.c=c;
+		}
+		
+		if(c<x.c)					x.left=put(x.left, key, val, d);
+		else if(c>x.c)				x.right=put(x.right, key, val, d);
+		else if(d<key.length()-1)	x.mid=put(x.mid, key, val, d+1);
+		else						x.value=val;
+		
+		return x;
 		
 	}
 	
