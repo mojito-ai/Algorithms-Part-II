83ee497401b39503d14ad66e07d600405311c453
mojito <msharma3@me.ittr.ac.in>
2021-02-15 11:34:31 +0530

    LRS API

--- 06. Radix Sorts/LongestRepeatedSubstring.java
+++ 06. Radix Sorts/LongestRepeatedSubstring.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
 * <h1> Longest Repeated Substring: Given a string of N characters, find the longest repeated substring
 * 
@@ -14,5 +16,25 @@
 */
 
 public class LongestRepeatedSubstring {
+	
+	public String lrs(String s)
+	{
+		int N=s.length();
+		String [] suffixes=new String[N];
+		
+		for(int i=0; i<N; i++)
+			suffixes[i]=s.substring(i, N);
+		
+		Arrays.sort(suffixes);
+		String lrs="";
+		
+		for(int i=0; i<N-1; i++)
+		{
+			int len=lcp(suffixes[i], suffixes[i+1]);
+			if(len>lrs.length())
+				lrs=suffixes[i].substring(0, len);
+		}
+		return lrs;
+	}
 
 }
