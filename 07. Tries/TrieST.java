ed7e2090c4b778ab7ac7d65a2fdf7983ee882fe0
mojito <msharma3@me.ittr.ac.in>
2021-02-15 21:23:00 +0530

    Trie (Review Character compares)

--- 07. Tries/TrieST.java
+++ 07. Tries/TrieST.java
@@ -3,12 +3,8 @@
 * 
 * <li> Summary of performance of symbol table implementations
 * 
-* <li> Order of growth of frequency of operations (key compares)
-* <li> Implementation		Search		Insert		Delete			Ordered Ops			Operation on keys
-* <li>	Red-Black BST		log N		 log N		 log N				Yes					compareTo()
-* <li>  HashTable			 1*			   1*		   1*				No					equals() hashCode()		* under uniform hashing assumption
 * 
-* 
+* <li> 
 * @author  Mohit Sharma
 * @version 1.0
 * @since   15-02-2021
@@ -17,4 +13,23 @@
 
 public class TrieST {
 
+	
+	/*
+	 * 
+	 * 1. Order of growth of frequency of operations (key compares)
+	 * Implementation		Search		Insert		Delete			Ordered Operation			Operation on keys
+	 * Red-Black BST		log N		 log N		 log N				Yes					compareTo()
+	 * HashTable			 1*			   1*		   1*				 No					equals() & hashCode()		* under uniform hashing assumption
+	 * 
+	 * 2. Order of growth of frequency of operations (character compares)
+	 * Implementation		Search hit	 Search miss		Insert			Space	
+	 * Red-Black BST		L+clog N		clog N			 clog N			 4N		
+	 * HashTable				L			 L					L			4N to 16N	
+	 * 
+	 * N=number of strings
+	 * L=length of string
+	 * R=radix
+	 * 
+	 * 	
+	 */
 }
