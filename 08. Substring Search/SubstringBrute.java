4fecd98d69673f701ea3ac39316eddb301f80da5
mojito <msharma3@me.ittr.ac.in>
2021-02-17 21:42:59 +0530

    Brute Force Substring Search

---
+++ 08. Substring Search/SubstringBrute.java
@@ -0,0 +1,24 @@
+/**
+* <h1> <b>Brute Force Substring Search:</b> Check for pattern starting at each position </h1>
+* 
+* <li> Pattern: A B R A
+* 
+* <li> i j i+j	0 1 2 3 4 5 6 7 8 9 10
+* <li> 		    A B A C A D A B R A C
+* <li> 0 2  2	A B R 
+* <li> 1 0  2	  A	
+* <li> 2 1  2	  	A B
+* <li> 3 0  2	  	  A	
+* <li> 4 1  2	  		A B
+* <li> 5 0  2	  		  A
+* <li> 6 4  2	  			A B R A
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   17-02-2021
+* 
+*/
+
+public class SubstringBrute {
+
+}
