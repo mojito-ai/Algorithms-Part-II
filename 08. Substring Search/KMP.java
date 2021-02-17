90af3781c8ae7d310f59aee08393e66574270d3c
mojito <msharma3@me.ittr.ac.in>
2021-02-17 22:12:55 +0530

    Knuth-Morris-Pratt

---
+++ 08. Substring Search/KMP.java
@@ -0,0 +1,17 @@
+/**
+* <h1> <b>Knuth-Morris-Pratt: </b> Ingenious way to do substring search without backup. </h1>
+* 
+* <li> Suppose that we are searching in text for pattern BAAAAAAAAA
+* <li> Suppose we match 5 chars in pattern, with mismatch on 6th char
+* <li> We know previous 6 chars are BAAAAB
+* <li> Don't need to backup pointer completely.
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   17-02-2021
+* 
+*/
+
+public class KMP {
+
+}
