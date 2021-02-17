78e1c8ba6fc30c99eb4c9eab2f5138c1afb46e58
mojito <msharma3@me.ittr.ac.in>
2021-02-17 19:09:10 +0530

    Substring Search Applications

--- 08. Substring Search/StockQuote.java
+++ 08. Substring Search/StockQuote.java
@@ -4,6 +4,13 @@
 * <li> Pattern : N E E D L E
 * <li> Text : I N A H A Y S T A C K N E E D L E I N A
 * 
+* <li> Applications: 
+* <li> 1. Find and Replace
+* <li> 2. Computer Forensics: Search memory or disk for signatures. (e.g. all URLs or RSA keys that the user has entered)
+* <li> 3. Identify patterns indicative of SPAM : PROFITS, LOSE WEIGHT, THIS IS A ONE TIME MAILING
+* <li> 4. Electronic surveillance: Building a machine to find "Attack at Dawn"
+* <li> 5. Screen Scraping: Extract relevant data from a web page
+* 
 * 
 * @author  Mohit Sharma
 * @version 1.0
