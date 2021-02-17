5bbc77f298bb00dc7b49e1608b7d266cbd15cb56
mojito <msharma3@me.ittr.ac.in>
2021-02-17 19:06:14 +0530

    Substring Search

---
+++ 08. Substring Search/StockQuote.java
@@ -0,0 +1,16 @@
+/**
+* <h1> Substring Search: Find pattern of length M in a text of length N. (Typically N>>M) </h1>
+* 
+* <li> Pattern : N E E D L E
+* <li> Text : I N A H A Y S T A C K N E E D L E I N A
+* 
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   17-02-2021
+* 
+*/
+
+public class StockQuote {
+
+}
