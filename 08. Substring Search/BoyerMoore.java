9912e2094d544242f72325eb3beabebf079dd6f1
mojito <msharma3@me.ittr.ac.in>
2021-02-18 23:35:37 +0530

    Boyer-Moore Substring Search

---
+++ 08. Substring Search/BoyerMoore.java
@@ -0,0 +1,17 @@
+/**
+* <h1> <b>Boyer-Moore:</b> Mismatched character heuristic  </h1>
+* 
+* <li> Robert Boyer & J. Strother Moore
+* <li> Intuition: 
+* <li> Scan characters in pattern from right to left.
+* <li> Can skip as many as M text chars when finding one not in pattern
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   18-02-2021
+* 
+*/
+
+public class BoyerMoore {
+
+}
