61fb99709405cf463d1f979f38ca55e233fed831
mojito <msharma3@me.ittr.ac.in>
2021-02-19 12:36:27 +0530

    Rabin-Karp

---
+++ 08. Substring Search/RabinKarp.java
@@ -0,0 +1,15 @@
+/**
+* <h1> <b>Rabin-Karp Fingerprint Search:</b> Basic idea of modular hashing</h1>
+* 
+* <li> Michael Rabin, Turing Award '76
+* <li> Dick Karp, Turing Award '85
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   19-02-2021
+* 
+*/
+
+public class RabinKarp {
+
+}
