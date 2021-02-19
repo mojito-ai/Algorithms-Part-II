7f52aee645576033d9a8a82b92ada1e9320ceaaf
mojito <msharma3@me.ittr.ac.in>
2021-02-19 12:40:03 +0530

    Rabin-Karp Fingerprint Search

--- 08. Substring Search/RabinKarp.java
+++ 08. Substring Search/RabinKarp.java
@@ -1,8 +1,13 @@
 /**
-* <h1> <b>Rabin-Karp Fingerprint Search:</b> Basic idea of modular hashing</h1>
+* <h1> <b>Rabin-Karp Fingerprint Search:</b> Basic idea of modular hashing. Easiest to think in context of numbers although
+* 											 it works in all kinds of situations as everything is encoded as a bit which can
+* 											 be treated as binary numbers.</h1>
 * 
 * <li> Michael Rabin, Turing Award '76
 * <li> Dick Karp, Turing Award '85
+* <li> Compute a hash of pattern characters 0 to M-1.
+* <li> For each i, compute a hash of text characters i to M+i-1
+* <li> If pattern hash = text substring hash, check for a match
 * 
 * @author  Mohit Sharma
 * @version 1.0
