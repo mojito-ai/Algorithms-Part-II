81403dada45488286ed968e4127f155bba741145
mojito <msharma3@me.ittr.ac.in>
2021-02-23 22:54:02 +0530

    NFA

---
+++ 09. Regular Expressions/NFA.java
@@ -0,0 +1,22 @@
+/**
+* <h1> <b>NFA:</b> Nondeterministic finite state automaton.</h1>
+* 
+* <li> 
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   23-02-2021
+* 
+*/
+
+public class NFA {
+	
+	/*
+	 * Duality between REs and DFAs: 
+	 * 
+	 * RE: Concise way to describe a set of strings.
+	 * DFA: Machine to recognise whether a given string is in a given set.
+	 * 
+	 */
+
+}
