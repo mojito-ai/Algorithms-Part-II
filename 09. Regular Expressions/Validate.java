ee2aff1684ee6dffe588aec5ebd9a3ee8ebf10a5
mojito <msharma3@me.ittr.ac.in>
2021-02-24 18:42:34 +0530

    Validate

---
+++ 09. Regular Expressions/Validate.java
@@ -0,0 +1,16 @@
+/**
+* <h1> <b>Validate:</b> Regular expressions in Java.</h1>
+* 
+* <li> Validity checking : Does the input match the regexp?
+* <li> Java String Library : Use input.matches(regexp) for basic RE matching. 
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   24-02-2021
+* 
+*/
+
+
+public class Validate {
+
+}
