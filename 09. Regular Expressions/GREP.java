754b933fe009e4bdb0c7b9f6a88511cebc1e1e3a
mojito <msharma3@me.ittr.ac.in>
2021-02-24 18:35:40 +0530

    GREP Running time

--- 09. Regular Expressions/GREP.java
+++ 09. Regular Expressions/GREP.java
@@ -3,7 +3,8 @@ import edu.princeton.cs.algs4.StdIn;
 /**
 * <h1> <b>GREP:</b> Generalized Regular Expression Print.</h1>
 * 
-* <li> Grep. Take a RE as a command-line argument and print the lines from standard input having some substring that is matched by the RE.
+* <li> Grep: Take a RE as a command-line argument and print the lines from standard input having some substring that is matched by the RE.
+* <li> Bottom line: Worst case for grep (proportional to MN) is the same as for brute-force substring search.
 * 
 * @author  Mohit Sharma
 * @version 1.0
@@ -14,6 +15,7 @@ public class GREP {
 	
 	public static void main(String [] args)
 	{
+		//contains RE as a substring
 		String regexp = "(.*" + args[0] + "*.)";
 		NFA nfa = new NFA(regexp);
 		while(StdIn.hasNextLine())
