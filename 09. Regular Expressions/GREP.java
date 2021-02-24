335cf80e97de4e8b168fa6c0146ceec035fd0661
mojito <msharma3@me.ittr.ac.in>
2021-02-24 18:27:39 +0530

    Generalized regular expression print

---
+++ 09. Regular Expressions/GREP.java
@@ -0,0 +1,13 @@
+/**
+* <h1> <b>GREP:</b> Generalized Regular Expression Print.</h1>
+* 
+* <li> Grep. Take a RE as a command-line argument and print the lines from standard input having some substring that is matched by the RE.
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   24-02-2021
+* 
+*/
+public class GREP {
+
+}
