1d010a62045442028618bc3b7f3cca592c6cdb3a
mojito <msharma3@me.ittr.ac.in>
2021-02-26 21:58:25 +0530

    Data compression Context

--- 10. Data Compression/Compression.java
+++ 10. Data Compression/Compression.java
@@ -1,15 +1,21 @@
 /**
 * <h1> <b>Data Compression:</b> Various techniques and applications of data compression</h1>
 * 
-* <li> Compression reduces the size of a file:
+* <li> 1. Compression reduces the size of a file:
 * <li> To save space when storing it.
 * <li> To save time when transmitting it.
 * <li> Most files have lots of redundancy.
 * 
-* <li> Who needs compression?
+* <li> 2. Who needs compression?
 * <li> Moore's law: # transistors on a chip doubles every 18–24 months.
 * <li> Parkinson's law: data expands to fill space available.
 * <li> Text, images, sound, video, …
+* 
+* <li> “ Everyday, we create 2.5 quintillion bytes of data—so much that
+* 		 90% of the data in the world today has been created in the last
+* 		 two years alone. ” — IBM report on big data (2011)
+* 
+* <li> Basic concepts ancient (1950s), best technology developed recently.
 * 
 * @author  Mohit Sharma
 * @version 1.0
