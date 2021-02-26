dd9880cdbbd6f80f35c7f19a6bbee5906148a9c6
mojito <msharma3@me.ittr.ac.in>
2021-02-26 21:42:17 +0530

    Data Compression

---
+++ 10. Data Compression/Compression.java
@@ -0,0 +1,16 @@
+/**
+* <h1> <b>Data Compression:</b> Various techniques and applications of data compression</h1>
+* 
+* <li> Compression reduces the size of a file:
+* <li> To save space when storing it.
+* <li> To save time when transmitting it.
+* <li> Most files have lots of redundancy.
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   26-02-2021
+* 
+*/
+public class Compression {
+
+}
