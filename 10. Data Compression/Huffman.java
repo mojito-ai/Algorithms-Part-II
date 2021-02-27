4caa586aa963821cc75c1b9a89ed69787384d160
mojito <msharma3@me.ittr.ac.in>
2021-02-27 15:53:01 +0530

    Huffman Compression

---
+++ 10. Data Compression/Huffman.java
@@ -0,0 +1,17 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+/**
+* <h1> <b>Huffman Compression</b> By David Huffman</h1>
+* 
+* <li> 
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   27-02-2021
+* 
+*/
+
+public class Huffman {
+
+}
