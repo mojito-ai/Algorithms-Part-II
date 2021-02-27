989919cca51a8b5628b785d37ff8c76c8c1dbf5f
mojito <msharma3@me.ittr.ac.in>
2021-02-27 15:22:53 +0530

    Run Length encoding

--- 10. Data Compression/RunLength.java
+++ 10. Data Compression/RunLength.java
@@ -1,3 +1,20 @@
+/**
+* <h1> <b>Run-Length Coding</b> Removing simple type of redundancy in bitstream.</h1>
+* 
+* <li> Run length encoding: Simple type of redundancy in bitstream = Long runs of repeated bits.
+* <li> Representation: 4 bit counts to represent alternating runs of 0s and 1s:
+* 
+* <li> 0000000000000001111111000000011111111111 (16 bits instead of 40)
+* <li> 15 0s, then 7 1s, then 7 0s, then 11 1s.
+* 
+* <li> 1111 0111 0111 1011		(16 bits instead of 40)
+* <li>  15	  7    7    11
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   26-02-2021
+* 
+*/
 
 public class RunLength {
 
