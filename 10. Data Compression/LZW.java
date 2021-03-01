6761162034979bc87e6007185030d6b3b6d71ced
mojito <msharma3@me.ittr.ac.in>
2021-03-01 12:23:10 +0530

    Lempel-Ziv-Welch Compression

---
+++ 10. Data Compression/LZW.java
@@ -0,0 +1,17 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+/**
+* <h1> <b>LZW Compression:</b> By Abraham Lempel, Jacob Ziv & Terry Welch</h1>
+* 
+* <li> 
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   1-03-2021
+* 
+*/
+
+public class LZW {
+
+}
