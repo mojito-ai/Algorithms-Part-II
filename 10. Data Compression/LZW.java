02c536b6f868887c6e37bac8bab6e2bc0d292e71
mojito <msharma3@me.ittr.ac.in>
2021-03-01 12:33:11 +0530

    LZW Compression

--- 10. Data Compression/LZW.java
+++ 10. Data Compression/LZW.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
+import edu.princeton.cs.algs4.TST;
 
 /**
 * <h1> <b>LZW Compression:</b> By Abraham Lempel, Jacob Ziv & Terry Welch</h1>
@@ -31,9 +32,27 @@ public class LZW {
 
 	private static final int R=256; //extended ASCII
 	private static final int W=8;	//fixed length W-bit codewords
+	private static final int L=999;
 	
 	public static void compress()
 	{
+		String input = BinaryStdIn.readString();
+		TST<Integer> st = new TST<>();
+		for(int i=0; i<R; i++)
+			st.put(""+(char)i, i);
+		int code = R+1;
 		
+		while(input.length()>0)
+		{
+			String s=st.longestPrefixOf(input);
+			BinaryStdOut.write(st.get(s),W);
+			int t=s.length();
+			if(t<input.length() && code<L)
+				st.put(input.substring(0,t+1), code++);
+			input=input.substring(t);
+		}
+		
+		BinaryStdOut.write(R,W);
+		BinaryStdOut.close();
 	}
 }
