c2735b39e4486453d91983afb40874ae20d1dc2e
mojito <msharma3@me.ittr.ac.in>
2021-03-01 12:24:29 +0530

    Static Model

--- 10. Data Compression/LZW.java
+++ 10. Data Compression/LZW.java
@@ -4,7 +4,12 @@ import edu.princeton.cs.algs4.BinaryStdOut;
 /**
 * <h1> <b>LZW Compression:</b> By Abraham Lempel, Jacob Ziv & Terry Welch</h1>
 * 
-* <li> 
+* <li> Statistical methods
+* 
+* <li> 1. Static model. Same model for all texts.
+* <li> Fast.
+* <li> Not optimal: different texts have different statistical properties.
+* <li> Ex: ASCII, Morse code.
 * 
 * @author  Mohit Sharma
 * @version 1.0
