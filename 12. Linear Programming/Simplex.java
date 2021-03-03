f175ace26bcd32b1ba75624e30563a0ea2713b11
mojito <msharma3@me.ittr.ac.in>
2021-03-03 22:56:55 +0530

    Simplex Generic algorithm

--- 12. Linear Programming/Simplex.java
+++ 12. Linear Programming/Simplex.java
@@ -4,7 +4,10 @@
 * <li> Developed shortly after WWII in response to logistical problems, including Berlin airlift.
 * <li> Ranked as one of top 10 scientific algorithms of 20th century. 
 * 
-* <li> 
+* <li> Generic algorithm.
+* <li> Start at some extreme point.
+* <li> Pivot from one extreme point to an adjacent one. (never decreasing objective function)
+* <li> Repeat until optimal.
 * 
 * @author  Mohit Sharma
 * @version 1.0
