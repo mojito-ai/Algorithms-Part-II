91a63e04aa9154487fbe8a7dce50e2147de28956
mojito <msharma3@me.ittr.ac.in>
2021-03-03 23:02:06 +0530

    Simplex Algorithm: pivot 1

--- 12. Linear Programming/Simplex.java
+++ 12. Linear Programming/Simplex.java
@@ -32,10 +32,14 @@ public class Simplex {
 	 *2. Simplex algorithm: initialization 
 	 *
 	 *Initial basic feasible solution.
-	 *		Start with slack variables { SC , SH , SM } as the basis.
+	 *		Start with slack variables { SC , SH , SM } as the basis. {one basic variable per row}
 	 *		Set non-basic variables A and B to 0.
 	 *		3 equations in 3 unknowns yields SC = 480, SH = 160, SM = 1190.
-
+ 	 *
+ 	 *
+ 	 *3. Simplex algorithm: pivot 1
+ 	 *
+ 	 *substitute B = (1/15) (480 – 5A – SC) and add B into the basis (rewrite 2nd equation, eliminate B in 1st, 3rd, and 4th equations)
 	 */
 
 }
