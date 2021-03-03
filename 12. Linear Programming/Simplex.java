6cd1bbbb78b6bf22bde1a459a5ccf622ef12a06a
mojito <msharma3@me.ittr.ac.in>
2021-03-03 22:55:54 +0530

    Simplex Algorithm

---
+++ 12. Linear Programming/Simplex.java
@@ -0,0 +1,17 @@
+/**
+* <h1> <b>Simplex Algorithm</b> Simplex algorithm. [George Dantzig, 1947] </h1>
+* 
+* <li> Developed shortly after WWII in response to logistical problems, including Berlin airlift.
+* <li> Ranked as one of top 10 scientific algorithms of 20th century. 
+* 
+* <li> 
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   03-03-2021
+* 
+*/
+
+public class Simplex {
+
+}
