ea9dc907dee869ea58cd129d99b37bed0c017c0f
mojito <msharma3@me.ittr.ac.in>
2021-03-09 16:25:41 +0530

    HamiltonPath DFS

--- 13. Intractability/HamiltonPath.java
+++ 13. Intractability/HamiltonPath.java
@@ -40,11 +40,21 @@ public class HamiltonPath {
 	
 	public HamiltonPath(Graph G)
 	{
-		
+		marked=new boolean[G.V()];
+		for(int v=0; v<G.V(); v++)
+			dfs(G,v,1);
 	}
 	
 	private void dfs(Graph G, int v, int depth)
 	{
+		marked[v]=true;
+		if(depth==G.V())
+			count++;
 		
+		for(int w: G.adj(v))
+			if(!marked[w])
+				dfs(G,w,depth+1);
+		
+		marked[v]=false;
 	}
 }
